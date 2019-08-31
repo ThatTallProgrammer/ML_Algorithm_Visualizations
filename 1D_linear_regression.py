b3e2065145122e952eb2a4880a4a808ec235603e
thattallprogrammer <thattallprogrammer@gmail.com>
2019-08-31 13:26:25 -0700

    Added comments and standardized output of 1D linear regression and 2D linear regression programs

--- 1D_linear_regression.py
+++ 1D_linear_regression.py
@@ -1,4 +1,11 @@
-import time
+"""
+Example program for calculating the slope
+of a line of best fit using gradient descent
+
+Joseph Cauthen
+August 31, 2019
+"""
+
 
 import numpy as np
 import statistics as stats
@@ -6,17 +13,24 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 
 
-def plot_graph(x, y, trend_line, line):
+def plot_graph(X, Y, line):
+    """
+    Plot a graph of the current model line
+
+    :param X: The known x values
+    :param Y: The knwon f(x) values
+    :param line: The matplotlib.lines.Line2D object of the
+        model line
+    :return: None
+    """
+
     # create the graph
     fig, ax = plt.subplots()
 
     # plot X and Y points
     ax.scatter(X, Y, c='black')
 
-    # add line of best fit
-    ax.add_line(trend_line)
-
-    # add additional line
+    # add line
     ax.add_line(line)
 
     # show the graph
@@ -25,29 +39,41 @@ def plot_graph(x, y, trend_line, line):
     plt.show()
 
 
-def calc_loss(x, y, slope, y_int):
-    loss = 0
+def calc_gradient(X, Y, slope, y_int):
+    """
+    Calculate the gradient as the L'(slope)
 
-    for i in range(len(x)):
-        loss += (y[i] - y_int - slope * x[i]) ** 2
+    :param X: The known x values
+    :param Y: The known f(x) values
+    :param slope: The current slope of the model line
+    :param y_int: The current y-int of the model line
+    :return: The derivative of the loss function at
+        the current slope
+    """
 
-    return loss / len(x)
+    grad = 0
+
+    for i in range(len(X)):
+        grad += -2 * X[i] * (Y[i] - y_int - slope * X[i])
+
+    return grad / len(X)
 
 
-def calc_gradient(x, y, slope, y_int):
+def calc_trend_slope(X, Y):
+    """
+    Calculate the slope of the line of best
+    fit for points (x, f(x)) using the statistical
+    function
 
-    gradient = 0
+    slope = cov(x, y) / var(x)
 
-    for i in range(len(x)):
-        gradient += -2 * x[i] * (y[i] - y_int - slope * x[i])
-
-    return gradient / len(x)
-
-
-def calc_trend_slope(x, y):
+    :param X: The known x values
+    :param Y: The known f(x) values
+    :return: The slope of the line of best fit
+    """
 
     # calculate the covariance matrix
-    cov_mat = np.cov(x, y)
+    cov_mat = np.cov(X, Y)
 
     # get covariance of X, Y and variance of X
     cov = cov_mat[0][1]
@@ -56,64 +82,94 @@ def calc_trend_slope(x, y):
     return cov / var_x
 
 
-def calc_trend_y_intercept(trend_slope, x, y):
+def calc_trend_y_intercept(X, Y):
+    """
+    Calculate the y-int of the line of best
+    fit using the statistical function
+
+    y-int = y-avg - slope * x-avg
+
+    :param X: List of known x values
+    :param Y: List of known f(x) values
+    :return: The y-int of the line of best fit
+    """
 
     # calculate the x and y averages
-    x_avg = stats.mean(x)
-    y_avg = stats.mean(y)
+    x_avg = stats.mean(X)
+    y_avg = stats.mean(Y)
+
+    # calculate the trend slope
+    m = calc_trend_slope(X, Y)
 
     return y_avg - trend_slope * x_avg
 
 
 def get_1d_linear_output(x, m, b):
+    """
+    Return the output of the linear
+    function
+
+    f(x) = m * x + b
+    :param x: The x being evaluated
+    :param m: The slope of the linear function
+    :param b: The y-int of the linear function
+    :return: f(x)
+    """
+
     return m * x + b
 
 
 if __name__ == "__main__":
+    # specify the known data
     X = [3, 5, 10, 20, 25]
     Y = [1, 3, 5, 7, 10]
-    step_size = 0.002
-    model_slope = 2
+
+    # specify the training parameters
+    gamma = 0.001
+    eps = 0.0001
+    model_slope = 10
+    max_iter = 10000
 
     # calculate line of best fit constants
     trend_slope = calc_trend_slope(X, Y)
-    trend_y_int = calc_trend_y_intercept(trend_slope, X, Y)
+    trend_y_int = calc_trend_y_intercept(X, Y)
 
-    while True:
+    # display model line before training
+    x_max = max(X)
+    model_line = lines.Line2D(
+        [0, x_max],
+        [trend_y_int, get_1d_linear_output(x_max, model_slope, trend_y_int)],
+        color='blue'
+    )
+    plot_graph(X, Y, model_line)
+
+    # train the model
+    for i in range(max_iter):
+        # calculate gradient for current model
         gradient = calc_gradient(X, Y, model_slope, trend_y_int)
-        loss = calc_loss(X, Y, model_slope, trend_y_int)
 
+        # update user with information
         print(
-            "Slope: {} Y_Int: {} Loss: {} Gradient: {}".format(
+            "{}) Slope: {} Y_Int: {} Gradient: {}".format(
+                i + 1,
                 model_slope,
                 trend_y_int,
-                loss,
                 gradient
             )
         )
 
-        # generate line of best fit
-        trend_line = lines.Line2D(
-            [0, 25],
-            [trend_y_int, get_1d_linear_output(25, trend_slope, trend_y_int)],
-            color='red'
-        )
-
-        # generated model line
-        model_line = lines.Line2D(
-            [0, 25],
-            [trend_y_int, get_1d_linear_output(25, model_slope, trend_y_int)],
-            color='blue'
-        )
-
-        plot_graph(X, Y, trend_line, model_line)
-
-        if abs(gradient) < 0.001:
+        # exit training if model is sufficiently accurate
+        if abs(gradient) < eps:
             break
 
-        step = (-1) * step_size * gradient
+        # move opposite the direction of the gradient
+        step = (-1) * gamma * gradient
         model_slope = model_slope + step
 
-        time.sleep(2)
-
-
+    # display the model line after training
+    model_line = lines.Line2D(
+        [0, x_max],
+        [trend_y_int, get_1d_linear_output(x_max, model_slope, trend_y_int)],
+        color='blue'
+    )
+    plot_graph(X, Y, model_line)
--- 2D_linear_regression.py
+++ 2D_linear_regression.py
@@ -1,3 +1,12 @@
+"""
+Example program for calculating the slope and
+y-int for a line of best fit using gradient
+descent
+
+Joseph Cauthen
+August 31, 2019
+"""
+
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 import numpy as np
@@ -6,6 +15,15 @@ from math import sqrt
 
 
 def plot_graph(X, Y, line):
+    """
+    Plot and display a graph of model
+
+    :param X: Known x values
+    :param Y: Known f(x) values
+    :param line: matplotlib.lines.Line2D object of model line
+    :return: None
+    """
+
     # create the graph
     fig, ax = plt.subplots()
 
@@ -22,7 +40,17 @@ def plot_graph(X, Y, line):
 
 
 def calc_grad_of_slope(X, Y, m, b):
+    """
+    Calculate the derivative of the loss
+    function with respect to the slope
 
+    :param X: List of known x values
+    :param Y: List of known f(x) values
+    :param m: Slope of the current model line
+    :param b: y-int of the current model line
+    :return: the derivative of the loss function
+        with respect to m
+    """
     grad = 0
 
     for i in range(len(X)):
@@ -32,6 +60,17 @@ def calc_grad_of_slope(X, Y, m, b):
 
 
 def calc_grad_of_intercept(X, Y, m, b):
+    """
+    Calculate the derivative of the loss
+    function with respect to the slope
+
+    :param X: List of known x values
+    :param Y: List of known f(x) values
+    :param m: Slope of the current model line
+    :param b: y-int of the current model line
+    :return: the derivative of the loss function
+        with respect to m
+    """
 
     grad = 0
 
@@ -42,10 +81,30 @@ def calc_grad_of_intercept(X, Y, m, b):
 
 
 def calc_grad_vec_magnitude(m_grad, b_grad):
+    """
+    Calculate the magnitude of the gradient
+    vector [L'(slope), L'(y-int)]
+
+    :param m_grad:
+    :param b_grad:
+    :return: the magnitude of the gradient vector
+    """
+
     return sqrt(m_grad ** 2 + b_grad ** 2)
 
 
 def calc_trend_y_intercept(X, Y):
+    """
+    Calculate the y-int for the line of best
+    fit for the points (x, f(x)) using the
+    statistical formula
+
+    y_int = y_avg - (cov(x, f(x)) / var(x)) * x_avg
+
+    :param X: List of known x values
+    :param Y: List of known f(x) values
+    :return: y-int of the line of best fit
+    """
 
     # calculate the mean of X and Y sets
     x_avg = np.mean(X)
@@ -54,10 +113,22 @@ def calc_trend_y_intercept(X, Y):
     # calculate the slope of line of best fit
     m = calc_trend_slope(X, Y)
 
+
     return y_avg - m * x_avg
 
 
 def calc_trend_slope(X, Y):
+    """
+    Calculate the slope of the line of best
+    fit for points (x, f(x)) using the statistical
+    formula
+
+    slope = cov(x, f(x)) / var(x)
+
+    :param X: List of known x values
+    :param Y: List of known f(x) values
+    :return: The slope of the line of best fit
+    """
 
     # calculate the covariance matrix
     cov_mat = np.cov(X, Y)
@@ -70,13 +141,25 @@ def calc_trend_slope(X, Y):
 
 
 def get_linear_output(x, slope, y_int):
+    """
+    Calculate the output for a linear function
+    at x given the slope and the y-intercept
+
+    f(x) = slope * x + y_int
+
+    :param x: The value being evaulated
+    :param slope: The slope of the line
+    :param y_int: The y_int of the line
+    :return: f(x)
+    """
+
     return slope * x + y_int
 
 
 if __name__ == "__main__":
     # set learning variables
     gamma = 0.001
-    e = 0.0001
+    eps = 0.0001
     model_slope = 3
     model_int = 10
     max_iter = 10000
@@ -94,6 +177,7 @@ if __name__ == "__main__":
     )
     plot_graph(X, Y, model_line)
 
+    # train the model
     for i in range(max_iter):
         # calculate the derivatives for m and b
         m_grad = calc_grad_of_slope(X, Y,
@@ -110,7 +194,7 @@ if __name__ == "__main__":
         ))
 
         # if the model is sufficiently fit, then exit loop
-        if abs(grad_mag) < e:
+        if abs(grad_mag) < eps:
             break
 
         # determine step size of variables
